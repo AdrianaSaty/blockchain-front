42afbdc87440ab0b3fc0df870cd58d79ec3d453a
Fernando Yto <fernando.toshioyto@gmail.com>
2019-12-02 23:39:31 -0300

    <feat> trying css table

--- src/components/molecules/SignupForm/SignupForm.js
+++ src/components/molecules/SignupForm/SignupForm.js
@@ -34,81 +34,86 @@ class Form extends Component {
   }
 
   render() {
-    console.log(this.state)
     return (
       <form onSubmit={this.handleSubmit} className='signup-form'>
-          <input
-            name='firstName'
-            placeholder='First Name'
-            type='text'
-            onChange={this.handleChange} />
-
-          <input
-            name='lastName'
-            placeholder='Last Name'
-            type='text'
-            onChange={this.handleChange} />
-
-          <label>
-            Date of Birth
-          </label>
-          <section>
+        <ul>
+          <li><span>
             <input
-              name='birthDay'
-              placeholder='Day'
-              type='number'
-              max={31}
-              min={1}
-              maxLength={2}
+              name='firstName'
+              placeholder='First Name'
+              type='text'
               onChange={this.handleChange} />
-
-            <select
-              name='birthMonth'
-              onChange={this.handleChange} > 
-              <option defaultValue='selected'>Month</option>
-              <option value='01'>January</option>
-              <option value='02'>February</option>
-              <option value='03'>March</option>
-              <option value='04'>April</option>
-              <option value='05'>May</option>
-              <option value='06'>June</option>
-              <option value='07'>July</option>
-              <option value='08'>August</option>
-              <option value='09'>September</option>
-              <option value='10'>October</option>
-              <option value='11'>November</option>
-              <option value='12'>December</option>
-            </select>
-
+          </span></li>
+          <li><span>
             <input
-              name='birthYear'
-              placeholder='Year'
-              type='number'
-              max={new Date().getFullYear() - 18}
-              min={1900}
-              maxLength={4}
+              name='lastName'
+              placeholder='Last Name'
+              type='text'
               onChange={this.handleChange} />
-          </section>
+          </span></li>
+          <li><span>
+              <label>
+                Date of Birth
+              </label>
+              <input
+                name='birthDay'
+                placeholder='Day'
+                type='number'
+                max={31}
+                min={1}
+                maxLength={2}
+                onChange={this.handleChange} />
 
-          <input
-            name='email'
-            placeholder='Email'
-            type='email'
-            onChange={this.handleChange} />
+              <select
+                name='birthMonth'
+                onChange={this.handleChange} > 
+                <option defaultValue='selected'>Month</option>
+                <option value='01'>January</option>
+                <option value='02'>February</option>
+                <option value='03'>March</option>
+                <option value='04'>April</option>
+                <option value='05'>May</option>
+                <option value='06'>June</option>
+                <option value='07'>July</option>
+                <option value='08'>August</option>
+                <option value='09'>September</option>
+                <option value='10'>October</option>
+                <option value='11'>November</option>
+                <option value='12'>December</option>
+              </select>
 
-          <input
-            name='password'
-            placeholder='Password'
-            type='password'
-            onChange={this.handleChange} />
-
-          <input
-            name='confirmPassword'
-            placeholder='Confirm Password'
-            type='password'
-            onChange={this.handleChange} />
-
-          <button type='submit'>Signup</button>
+              <input
+                name='birthYear'
+                placeholder='Year'
+                type='number'
+                max={new Date().getFullYear() - 18}
+                min={1900}
+                maxLength={4}
+                onChange={this.handleChange} />
+            </span></li>
+          <li><span>
+            <input
+              name='email'
+              placeholder='Email'
+              type='email'
+              onChange={this.handleChange} />
+          </span></li>
+          <li><span>
+            <input
+              name='password'
+              placeholder='Password'
+              type='password'
+              onChange={this.handleChange} />
+          </span></li>
+          <li><span>
+            <input
+              name='confirmPassword'
+              placeholder='Confirm Password'
+              type='password'
+              onChange={this.handleChange} />
+          </span></li>
+        </ul>
+        <button type='submit'>Signup</button>
       </form>
     );
   }
