9a7acc1efd538b686ce601abcb4a09f1eae37df6
Fernando Yto <fernando.toshioyto@gmail.com>
2019-12-17 01:27:58 -0300

    <fix> working on home page

---
+++ src/components/molecules/BetForm/BetForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BetForm = () => {
+  return (
+    <form>
+      <input type='Number' >Amount</input>
+    </form>
+  );
+}
+
+export default BetForm;
--- src/service/football-api.js
+++ src/service/football-api.js
@@ -1,19 +1,9 @@
 const axios = require('axios');
 
-// async function currentMatchday() {
-//   const response = await axios.get(apiUrl, { headers: { 'X-Auth-Token': '39bdb2d1264e45d99b7cef9897f17968' } });
-//   return response.data.currentSeason.currentMatchday;
-// }
-
-async function getMatches(matchday, user) {
+async function getMatches(matchday) {
   try {
-    const matches =  await axios.get(`${process.env.REACT_APP_BLOCKCHAIN_LOGIN_API}/games/matchday-games`, {
-      params: {
-        matchday,
-        token: user.token,
-      }
-    });
-    console.log(matches);
+    const matches =  await axios.get(`${process.env.REACT_APP_BLOCKCHAIN_LOGIN_API}/games/matchday-games?matchday=${matchday}`);
+    return matches.data;
   } catch (error) {
     console.log(error);
   }
