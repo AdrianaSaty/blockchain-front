f7924edc758157a55cd0fbbe70ff7c8c92980079
Adriana Saty Bertelli <Adria@adria-2.partner.bs.br.bsch>
2019-12-18 10:56:48 -0300

    <feat> loading balance

--- src/components/molecules/Loading/Loading.js
+++ src/components/molecules/Loading/Loading.js
@@ -6,7 +6,7 @@ class Loading extends Component {
     render() {
         return (
             <div>
-                <div className="container" id="preloader">
+                <div className="" id="preloader">
                     <div className="lds-ring"><div></div><div></div><div></div><div></div>	</div>
                 </div>
                 <div id="content"></div>
